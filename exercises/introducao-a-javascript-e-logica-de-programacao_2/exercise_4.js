57f60228c186e403e59b5ff8ca6ef26eb18d9b50
cristianocsl <cslcristiano@gmail.com>
2021-04-24 15:27:08 -0300

    Exercício 4 corrigido.

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_4.js
@@ -1,13 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-/*Utilizando for , descubra qual o maior valor 
-contido no array e imprima-o;*/
-
-let maiorAtual = numbers[0];
-
-for (let index = 1; index < numbers.length; index +=1) {
-    
-    if (numbers[index] > maiorAtual) {
-        maiorAtual = numbers[index];
-    } else {}
+/*Com o mesmo código do exercício anterior, 
+caso o valor final seja maior que 20, 
+imprima a mensagem: "valor maior que 20". 
+Caso não seja, imprima a mensagem: "valor 
+menor ou igual a 20";*/
+let soma = 0;
+for (let index = 0; index < numbers.length; index +=1) {
+    soma = soma + numbers[index];
     }
-    console.log(maiorAtual)
+    let media = soma / numbers.length;
+if (media > 20) {
+    console.log(`Valor maior que 20, pois o valor da média é ${media}.`);
+} else {
+    console.log('Valor menor que 20');
+}
