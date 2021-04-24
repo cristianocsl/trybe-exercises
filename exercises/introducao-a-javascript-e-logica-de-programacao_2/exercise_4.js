a85088e564dd67530e2415795627df211d10ac72
cristianocsl <cslcristiano@gmail.com>
2021-04-24 15:20:57 -0300

    modifica exercício 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_4.js
@@ -2,12 +2,12 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 /*Utilizando for , descubra qual o maior valor 
 contido no array e imprima-o;*/
 
-let maior = numbers[0];
+let maiorAtual = numbers[0];
 
 for (let index = 1; index < numbers.length; index +=1) {
     
-    if (numbers[index] > maior) {
-        maior = numbers[index];
+    if (numbers[index] > maiorAtual) {
+        maiorAtual = numbers[index];
     } else {}
     }
-    console.log(maior)
+    console.log(maiorAtual)
