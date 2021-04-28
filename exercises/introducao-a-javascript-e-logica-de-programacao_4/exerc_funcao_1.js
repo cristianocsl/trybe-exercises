b0b02e1e6a06745ad4604a44f181d55b650834aa
cristianocsl <cslcristiano@gmail.com>
2021-04-28 14:09:49 -0300

    Falta terminar a parte de inverter palavras

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exerc_funcao_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exerc_funcao_1.js
@@ -1,12 +1,13 @@
-
+let string = 'arara';
 function verificarPalindromo (string) { //aqui foi criada a função que recebe uma string
-let inverte = string.reverse();
-
-    if (string == inverte) {
+    let stringSepare = string.split();
+    let stringReverse = stringSepare.reverse();
+console.log(stringReverse);
+for (let index = 0; index < stringReverse.length; index += 1) {
+    if (string[index] == stringReverse[index]) {
         console.log('true');
     } else {
         console.log('false');
-    };
+    }
+}
 };
-
-console.log(verificarPalindromo(arara))
