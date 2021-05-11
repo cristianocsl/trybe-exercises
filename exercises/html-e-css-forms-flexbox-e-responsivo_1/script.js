7133585b59ff13f84fed074bec9ecf07961ae224
cristianocsl <cslcristiano@gmail.com>
2021-05-11 20:26:18 -0300

    botão limpar concluído

--- exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,1 +1,12 @@
-const botaoLimpar = document.getElementById('limpar');
+const divPai = document.querySelectorAll('div');
+
+function eraseButton() {
+  const arrayInput = document.getElementsByTagName('input');
+  for (let input = 0; input < arrayInput.length; input += 1) {
+    const element = arrayInput[input];
+    divPai.removeChild(element);
+  }
+}
+
+const clickButtonErase = document.getElementById('limpar');
+clickButtonErase.addEventListener('click', eraseButton);
