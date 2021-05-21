6715ca0be009c40aa8e9ea8974374f75712a92c9
cristianocsl <cslcristiano@gmail.com>
2021-05-21 20:58:18 -0300

    exercçio 2 - teste unitário

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/testeUnitario-2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/testeUnitario-2.js
@@ -10,9 +10,28 @@ function myRemove(arr, item) {
   for (let index = 0; index < arr.length; index += 1) {
     if (item !== arr[index]) {
       newArr.push(arr[index]);
+      // throw Error('Não retorna o array esperado.')
     }
   }
   return newArr;
 }
 
-// implemente seus testes aqui
+array1 = [1, 2, 3, 4];
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// const resultado = myRemove(array1,3);
+// assert.deepStrictEqual(resultado, [1, 2, 4], 'O resultado esperado é [1, 2, 4], meu amigo!');
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// const resultado = myRemove(array1,3);
+// assert.deepStrictEqual(resultado, [1, 2, 4], 'O resultado esperado é [1, 2, 4], meu amigo!');
+
+// Verifique se o array passado por parâmetro não sofreu alterações
+// const resultado = myRemove(array1,3);
+// assert.notDeepStrictEqual(resultado, [1, 2, 3, 4], 'O resultado esperado é [1, 2, 3, 4], meu amigo!');
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado. NÃO ENTENDI ESSA QUESTÃO.
+const myList = [5, 6, 7, 8]; // DE ONDE VEM ESSA ARRAY? A QUESTÃO NÃO DIZ.
+myRemove(myList, 5);
+assert.deepStrictEqual(myList, [5, 6, 7, 8]);
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
