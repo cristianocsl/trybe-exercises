c3ff8125a7fc7d19d6689228eef5ec3f8fdca04d
cristianocsl <cslcristiano@gmail.com>
2021-05-26 17:49:22 -0300

    exercicios do conteudo

--- exercises/higher-order-functions-do-javascript-es6_2/exercicio-conteudo-2.js
+++ exercises/higher-order-functions-do-javascript-es6_2/exercicio-conteudo-2.js
@@ -1,7 +1,57 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const multipliesFor2 = (element) => {
-  console.log(`${element} * 2: ${element * 2}`);
-};
+// const multipliesFor2 = (element) => {
+//   console.log(`${element} * 2 = ${element * 2}`);
+// };
 
-numbers.forEach(multipliesFor2);
+// numbers.forEach(multipliesFor2);
+
+// const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+// const convertToUpperCase = (name, index) => {
+//   names[index] = name.toUpperCase();
+//   console.log(names[index]);
+// };
+
+// names.forEach(convertToUpperCase);
+// console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+
+// 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
+
+// const emailListInData = [
+//   'roberta@email.com',
+//   'paulo@email.com',
+//   'anaroberta@email.com',
+//   'fabiano@email.com',
+// ];
+
+// const showEmailList = (email) => {
+//   console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+// };
+
+// // Adicione seu código aqui
+// emailListInData.forEach(showEmailList);
+
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const verifyEven = (number) => number % 2 === 0;
+
+// const isEven = numbers.find(verifyEven);
+
+// console.log(isEven); // 30
+
+// console.log(verifyEven(9)); // False
+// console.log(verifyEven(14)); // True
+
+// // Outra forma de ser realizada sem a necessidade de criar uma nova função.
+// const isEven2 = numbers.find((number) => number % 2 === 0);
+
+// console.log(isEven2); // 30
+
+// 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  return numbers.find((number) => number % 3 === 0 && number % 5 == 0);
+}
+console.log(findDivisibleBy3And5());
