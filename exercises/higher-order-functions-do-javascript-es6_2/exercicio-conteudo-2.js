89732efefb88ebd8f7f58faf7e8406eec0fa6338
cristianocsl <cslcristiano@gmail.com>
2021-05-26 19:16:12 -0300

    exercicio de exemplo do conteudo

--- exercises/higher-order-functions-do-javascript-es6_2/exercicio-conteudo-2.js
+++ exercises/higher-order-functions-do-javascript-es6_2/exercicio-conteudo-2.js
@@ -49,9 +49,44 @@
 // console.log(isEven2); // 30
 
 // 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
-const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
-  return numbers.find((number) => number % 3 === 0 && number % 5 == 0);
-}
-console.log(findDivisibleBy3And5());
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const findDivisibleBy3And5 = () => {
+//   return numbers.find((number) => number % 3 === 0 && number % 5 == 0);
+// }
+// console.log(findDivisibleBy3And5());
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// const findNameWithFiveLetters = () => { // porque aqui funciona mesmo sem definir parâmetro?
+//   return names.find((element) => element.length === 5); // mesmo dentro desta função, o array names é capturado? não entendi!
+// }
+
+// console.log(findNameWithFiveLetters()); // por que aqui funciona mesmo sem definir parâmetro?
+
+// // 3 - Utilize o find para encontrar a música com id igual a 31031685 , caso ela exista:
+
+// const musicas = [
+//   { id: '31031685', title: 'Partita in C moll BWV 997' },
+//   { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+//   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+// ]
+
+// function findMusic(id) { // esta função já tinha parâmentro. Por quê?
+//   return musicas.find((element) => element.id === id);
+// }
+
+// console.log(findMusic('31031685')) // por que precisou de parâmetro, mas no exercício 2, não?
+
+// 1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+
+// const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+// const hasName = (arr, name) => {
+//   return arr.some((currentName) => currentName === name);
+// }
+
+// console.log(hasName(names, 'Ana'))
