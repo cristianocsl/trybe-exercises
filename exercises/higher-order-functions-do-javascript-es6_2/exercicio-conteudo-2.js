6be65a7136dc3f76318514e006c0688e42cc07a3
cristianocsl <cslcristiano@gmail.com>
2021-05-26 19:29:21 -0300

    parte de fixaçao de conteudo concluido

--- exercises/higher-order-functions-do-javascript-es6_2/exercicio-conteudo-2.js
+++ exercises/higher-order-functions-do-javascript-es6_2/exercicio-conteudo-2.js
@@ -90,3 +90,19 @@
 // }
 
 // console.log(hasName(names, 'Ana'))
+
+// 2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((objeto) => objeto.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 18));
