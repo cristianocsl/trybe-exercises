151a47f3b6f3b14c50746ea0a976605f191d77ea
cristianocsl <cslcristiano@gmail.com>
2021-06-08 13:29:57 -0300

    exercicios de fixaçao

---
+++ exercises/javascript-assincrono-e-promises_1/javas-assincrono.js
@@ -0,0 +1,63 @@
+// const assert = require('assert');
+
+// const pushNumber = (list, number) => list.push(number);
+
+// let numbers = [];
+
+// setTimeout(() => pushNumber(numbers, 1), 3000);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
+
+// // Para passar no teste, é necessário testar o numbers após 3000 milissegundos:
+
+// const assert = require('assert');
+
+// const pushNumber = (list, number) => {
+//   list.push(number);
+//   console.log(list);
+// };
+
+// let numbers = [];
+
+// setTimeout(() => pushNumber(numbers, 1), 3000);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// setTimeout(() => assert.deepStrictEqual(numbers, [2, 3, 1]), 3000);
+
+const myExpenses = [
+  {
+    gym: 99,
+  },
+  {
+    ifood: 200,
+  },
+  {
+    phone: 60,
+  },
+  {
+    internet: 100,
+  },
+];
+
+const myIncome = 1000;
+
+const monthlyBudget = (myIncome, myExpenses, callback) => {
+
+  const totalExpenses = callback(myExpenses);
+  const totalAfterExpenses = myIncome - totalExpenses;
+
+  console.log(`Balanço do mês:
+    Recebido: R$${myIncome},00
+    Gasto: R$${totalExpenses},00
+    Saldo: R$${totalAfterExpenses},00 `);
+};
+const handleExpenses = myExpenses => {
+  const eachValue = myExpenses.map((item) => Object.values(item)[0]);
+  const totalExpense = eachValue.reduce((acc, curr) => acc + curr , 0);
+  return totalExpense;
+};
+
+monthlyBudget(myIncome, myExpenses, handleExpenses);
