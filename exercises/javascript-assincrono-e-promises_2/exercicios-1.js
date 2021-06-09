2343a55a1772a7e3e85c0224169e1f254ac8b2e8
cristianocsl <cslcristiano@gmail.com>
2021-06-09 11:17:16 -0300

    exercicio 1

---
+++ exercises/javascript-assincrono-e-promises_2/exercicios-1.js
@@ -0,0 +1,20 @@
+const fetchPromise = () => { // Por que a função de baixo deve ser envolvida pela arrow function 'fetchPromise' ?
+const promise = new Promise((resolve, reject) => {
+
+  const arrayRandon = [...Array(10)].
+  map(() => Math.floor(Math.random() * 50)).
+  map((item) => item * item);
+
+  const sum = arrayRandon.reduce((acc, current) => acc + current, 0);
+  console.log(`Resultado da soma: ${sum}
+Condição de Sucesso: soma < 8000`); // linha para conferir valor da soma.
+
+  sum < 8000 ? resolve() : reject(); // resolve e reject devem ser invocadas sem parâmetro
+});
+
+promise
+  .then(() => console.log(`Promise resolvida`))
+  .catch(() => console.log('Promise rejeitada'))
+};
+
+fetchPromise(); // a função de fora deve ser invocada
