9c7a9f5c8ac3ec51574b0fedc83ace6058925f6d
cristianocsl <cslcristiano@gmail.com>
2021-06-16 23:33:21 -0300

    exercicio 1 e 2

--- exercises/testes-automatizados-com-jest_3/exercicio-1.js
+++ exercises/testes-automatizados-com-jest_3/exercicio-1.js
@@ -1,5 +1,15 @@
 function aleatorio() {
-  return Math.random() * 100;
+  return Math.floor(Math.random() * 101);
 }
 
-module.exports = { aleatorio };
+const stringUpC = (string) => string.toUpperCase;
+
+const stringUpCFirst = (string) => {
+  // return string.substring(0, 1).toUpperCase().concat(string.substring(1))
+  const firstUppC = `${string[0].toUpperCase()}${string.slice(1)}`;
+  return firstUppC;
+}
+
+const stringConcat = (string1, string2) => `${string1}${string2}`;
+
+module.exports = { aleatorio, stringUpC, stringUpCFirst, stringConcat };
--- exercises/testes-automatizados-com-jest_3/exercicio-1.test.js
+++ exercises/testes-automatizados-com-jest_3/exercicio-1.test.js
@@ -1,9 +1,36 @@
-const { aleatorio } = require('./exercicio-1');
-const { expect } = require('@jest/globals');
+const arq = require('./exercicio-1');
 
 // Exercício 1
 test('Testa a função aleatório', () => {
-  aleatorio = jest.fn();
-  mockReturnValue(10);
-  expect(aleatorio()).toBe(10);
+  arq.aleatorio = jest.fn()
+    .mockReturnValue(10);
+
+  expect(arq.aleatorio()).toBe(10);
+  expect(arq.aleatorio).toHaveBeenCalled();
+  expect(arq.aleatorio).toHaveBeenCalledTimes(1);
 })
+
+// Exercício 2
+test('testando se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros', () => {
+  arq.aleatorio = jest.fn()
+    .mockImplementationOnce((a, b) => a / b);
+  
+  expect(arq.aleatorio(8, 2)).toBe(4);
+  expect(arq.aleatorio).toHaveBeenCalled();
+  expect(arq.aleatorio).toHaveBeenCalledTimes(1);
+  expect(arq.aleatorio).toHaveBeenCalledWith(8, 2);
+})
+
+// Exercício 3
+test('', () => {
+  arq.aleatorio = jest.fn()
+    .mockImplementationOnce((a, b, c) => a * b * c);
+  
+  expect(arq.aleatorio(2, 3, 2)).toBe(12);
+  expect(arq.aleatorio).toHaveBeenCalled();
+  expect(arq.aleatorio).toHaveBeenCalledTimes(1);
+  expect(arq.aleatorio).toHaveBeenCalledWith(2, 3, 2);
+})
+
+// Exercício 4
+
