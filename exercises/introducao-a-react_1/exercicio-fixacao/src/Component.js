6a4fa51641be3bc428278764510bbccda1a2044a
cristianocsl <cslcristiano@gmail.com>
2021-06-21 18:28:11 -0300

    exercicios de fixaçao

---
+++ exercises/introducao-a-react_1/exercicio-fixacao/src/Component.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// class HelloWorld extends React.Component {
+// }
+
+// Com destructuring
+import React from 'react'
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Cristiano Lima</h1>
+        <p>Navengando em novos mares... terra (Front-end) à vista!!!</p>
+      </div>
+    );
+  }
+}
+
+export default Component;
