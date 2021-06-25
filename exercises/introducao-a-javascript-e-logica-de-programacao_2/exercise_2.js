39b923914699a0a793ff8cc0522b59ac4e68bb6f
cristianocsl <cslcristiano@gmail.com>
2021-06-25 08:09:59 -0300

    arquivos removidos

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_2.js
+++
@@ -1,8 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-/*Para o segundo exercício, some todos os valores contidos 
-no array e imprima o resultado;*/
-let soma = 0;
-for (let index = 0; index < numbers.length; index +=1) {
-    soma = soma + numbers[index];
-    }
-    console.log(soma);
--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_3.js
+++
@@ -1,9 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-/*Para o terceiro exercício, calcule e imprima a média 
-aritmética dos valores contidos no array;*/
-let soma = 0;
-for (let index = 0; index < numbers.length; index +=1) {
-    soma = soma + numbers[index];
-    }
-    let media = soma / numbers.length;
-    console.log(media);
