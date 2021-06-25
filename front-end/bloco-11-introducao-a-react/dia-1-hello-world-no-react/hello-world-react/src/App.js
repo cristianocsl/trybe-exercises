fd246a05608811566109c5e5ac8294ce318f7268
cristianocsl <cslcristiano@gmail.com>
2021-06-25 06:02:09 -0300

    pastas do dia 1 do react bloco 11 na branch correta

---
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/hello-world-react/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return <HelloWorld /> // o componente HelloWorld foi renderizado dentre desta função.
+}
+
+export default App;
