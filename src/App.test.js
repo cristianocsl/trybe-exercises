13b4251ccb1bf5243e29d3952fe2f49545afa283
cristianocsl <cslcristiano@gmail.com>
2021-07-20 11:45:47 -0300

    testes cobrindo o statement de busca e o if da funçao requestDigimon

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('Teste da aplicação toda', () => {
-
-  it('renders App', async () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/Faça uma pesquisa/i);
+  beforeEach(() => render(<App />));
+  afterEach(() => jest.clearAllMocks);
+  
+  it('renderiza App', () => { // testa se input, botão e textos estão sendo renderizados corretamente em APP.
+    const linkElement = screen.getByText(/Search Digimon/i);
     expect(linkElement).toBeInTheDocument();
+    const makeASearch = screen.getByText(/faça uma pesquisa/i)
+    expect(makeASearch).toBeInTheDocument();
+    const searchInput = screen.getByTestId('input');
+    expect(searchInput).toBeInTheDocument();
+  });
+
+  it('insere um valor na caixa busca', () => { // teste que cobre o statement referente a linha 12 do App, que atualiza o state. Após encontrar o input, teste se event.target.value está sendo passado para o state "searchDigimon".
+    const input = screen.getByRole('textbox');
+    expect(input).toBeInTheDocument();
+    userEvent.type(input,'Digimonge');
+    expect(input).toHaveValue('Digimonge');
+  })
+
+  test('Insere um valor na caixa de busca', () => {
+
+    const input = screen.getByTestId('input');
+    expect(input).toHaveValue('');
+
+    userEvent.type(input, 'Agumon');
+    expect(input).toHaveValue('Agumon');
+
+    const button = screen.getByTestId('buttonSearch');
+    expect(button).toBeInTheDocument();
+    userEvent.click(button);
   });
 
 });
--- src/Digimon.test.js
+++ src/Digimon.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Digimon from './Digimon';
 
-describe('Teste da tela do Digimon', () => {
-  it('renders Digimon', async () => {
-
+describe.skip('Teste da tela do Digimon', () => {
+  it('renders Digimon', () => {
+    render(<Digimon />);
+    const digimonNameField = screen.getByTestId('digimonName');
+    expect(digimonNameField).toBeNull();
   });
 });
