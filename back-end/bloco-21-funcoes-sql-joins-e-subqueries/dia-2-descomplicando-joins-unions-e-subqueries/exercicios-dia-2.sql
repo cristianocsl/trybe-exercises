45b3034333e3c5381fbb4046b8ec8a33e951a115
cristianocsl <cslcristiano@gmail.com>
2021-09-19 13:30:13 -0300

    exercicios-bloco-20-dia-2

--- back-end/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercicios-dia-2.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercicios-dia-2.sql
@@ -51,7 +51,18 @@ ORDER BY T.name;
 
 -- Exercício 5: Utilizando o RIGHT JOIN , faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
 
-
+SELECT 
+    T.name,
+    T.location,
+    M.title,
+    M.director,
+    M.year,
+    M.length_minutes
+FROM
+    Pixar.Movies AS M
+RIGHT JOIN Pixar.Theater AS T
+ON T.id = M.theater_id
+ORDER BY T.name;
 
 select * from Pixar.Movies;
 select * from Pixar.Theater;
