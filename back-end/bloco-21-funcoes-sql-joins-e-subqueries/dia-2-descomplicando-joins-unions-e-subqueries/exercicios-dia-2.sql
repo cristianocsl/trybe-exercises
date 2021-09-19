604307ce9c2cc0324ec6fe3444a1586249d78074
cristianocsl <cslcristiano@gmail.com>
2021-09-19 13:31:55 -0300

    exercicios-bloco-20-dia-2-exercicio-5

--- back-end/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercicios-dia-2.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-subqueries/dia-2-descomplicando-joins-unions-e-subqueries/exercicios-dia-2.sql
@@ -51,7 +51,8 @@ ORDER BY T.name;
 
 -- Exercício 5: Utilizando o RIGHT JOIN , faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
 
-SELECT 
+SELECT
+    T.id,
     T.name,
     T.location,
     M.title,
