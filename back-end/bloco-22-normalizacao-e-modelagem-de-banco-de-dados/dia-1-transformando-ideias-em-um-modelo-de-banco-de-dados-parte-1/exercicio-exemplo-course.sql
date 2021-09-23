02717b9044384c09129b9589f828fecad8f606a8
cristianocsl <cslcristiano@gmail.com>
2021-09-23 19:35:27 -0300

    exercícios dos exemplos do course

--- back-end/bloco-22-normalizacao-e-modelagem-de-banco-de-dados/dia-1-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-1/exercicio-exemplo-course.sql
+++ back-end/bloco-22-normalizacao-e-modelagem-de-banco-de-dados/dia-1-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-1/exercicio-exemplo-course.sql
@@ -6,6 +6,28 @@ CREATE TABLE artista(
     artista_id INT /*pode ser assim também: INT(10)*/ NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE estiloMusical(
+    estilo_id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Album(
+    album_id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(60) NOT NULL,
+    preço INT NOT NULL,
+    estilo_id INT NOT NULL,
+    artista_id INT NOT NULL,
+    FOREIGN KEY (estilo_id) REFERENCES estiloMusical (estilo_id),
+    FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
+);
+
+CREATE TABLE cancao(
+    cancao_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    album_id INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES Album (album_id)
+);
 -- sintaxe criar tabela:
 
 -- CREATE TABLE nome_tabela(
@@ -14,6 +36,7 @@ CREATE TABLE artista(
 -- ) ENGINE = 'nome_engine' o mais comum é o InnoDB >> engine possibilita usar chaves estrangeiras, entre outras coisas.
 
 -- Criar tabela com chave composta:
+
 CREATE TABLE filme_ator(
     ator_id INTEGER,
     filme_id INTEGER,
