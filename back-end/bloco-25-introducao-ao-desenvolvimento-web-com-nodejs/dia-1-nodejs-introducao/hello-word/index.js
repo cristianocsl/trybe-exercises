2fa3faab29e72d4a75ae71fa1fc11a05a4d9c868
cristianocsl <cslcristiano@gmail.com>
2021-10-20 15:52:40 -0300

    exercicios de fixação

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/hello-word/index.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/hello-word/index.js
@@ -1,6 +1,6 @@
 const readline = require('readline-sync');
 
 const name = readline.question('Qual é o seu nome? ');
-const age = realind.questionInt('Qual é a sua idade? ');
+const age = readline.questionInt('Qual é a sua idade? ');
 
 console.log(`Hello, ${name}! You are ${age} years old!`);
