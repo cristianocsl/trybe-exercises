b98120302cc6ed6459f0717db0fa1a7abe4f07f8
cristianocsl <cslcristiano@gmail.com>
2021-10-21 16:39:41 -0300

    Atributos adicionais na utilização de métodos de leitura e escrita no fs

--- back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/writeFile.js
+++ back-end/bloco-25-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/writeFile.js
@@ -26,15 +26,23 @@ const fs = require('fs').promises;
 
 // Rodando promessas simultaneamente com Promise.all
 
-Promise.all([
-  fs.readFile('./arq1.txt'),
-  fs.readFile('./arq2.txt'),
-  fs.readFile('./arq3.txt'),
-])
-  .then(([arq1, arq2, arq3]) => {
+// Promise.all([
+//   fs.readFile('./arq1.txt'),
+//   fs.readFile('./arq2.txt'),
+//   fs.readFile('./arq3.txt'),
+// ])
+//   .then(([arq1, arq2, arq3]) => {
     
-    const fileSizeSum = arq1.byteLength + arq2.byteLength + arq3.byteLength;
+//     const fileSizeSum = arq1.byteLength + arq2.byteLength + arq3.byteLength;
 
-    console.log(`Tamanho total dos três arquivos: ${fileSizeSum}`);
-  })
-  .catch((err) => console.log(`Erro ao ler arquivos: ${err.message}`));
+//     console.log(`Tamanho total dos três arquivos: ${fileSizeSum}`);
+//   })
+//   .catch((err) => console.log(`Erro ao ler arquivos: ${err.message}`));
+
+// fs.writeFile('./novo-arq3.txt', 'Criei este arquivo e adicionei nele este conteúdo com a flag wx', { flag: 'wx' })
+//   .then(() => console.log('Novo arquivo criado e salvo!'))
+//   .catch((err) => console.error(`${err}`));
+
+fs.writeFile('./arq3.txt', 'Adicionei o conteúdo nesse arquivo com a flag wx', { flag: 'w' })
+  .then(() => console.log('Arquivo salvo'))
+  .catch((err) => console.error(`${err}`));
