29b474bfc164231970e6d723a926ca8cb52741b1
cristianocsl <cslcristiano@gmail.com>
2021-11-04 15:29:06 -0300

    exercicio de fixaçao dia 26.1

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Author = require('./models/Author');
 const Book = require('./models/Book');
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
@@ -13,7 +15,19 @@ app.get('/authors/:id', async (req, res) => {
   const author = await Author.findById(id);
   if (!author) return res.status(404).json({ message: 'Não encontrado' });
   res.status(200).json(author);
-})
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  const stringValidation = Author.isValid(first_name, middle_name, last_name);
+
+  if (!stringValidation) return res.status(400).json({ message: 'Dados inválidos' });
+
+  await Author.create(first_name, middle_name, last_name)
+
+  res.status(201).json({ message: 'Autor criado com sucesso!' });
+});
 
 app.get('/books', async (req, res) => {
   const books = await Book.getAll();
