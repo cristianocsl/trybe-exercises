1a5583b979bd2e9859f5acb6d9d45c937ac4d419
cristianocsl <cslcristiano@gmail.com>
2021-11-04 15:16:16 -0300

    exercicio de fixaçao dia 26.1

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Author = require('./models/Author');
-// const Book = require('./models/Book');
+const Book = require('./models/Book');
 const app = express();
 
 app.get('/authors', async (_req, res) => {
@@ -8,19 +8,23 @@ app.get('/authors', async (_req, res) => {
   res.status(200).json(authors);
 });
 
-// app.get('/books', async (req, res) => {
-//   const books = await Book.getAll();
-//   res.status(200).json(books);
-// });
-
 app.get('/authors/:id', async (req, res) => {
   const { id } = req.params;
-
   const author = await Author.findById(id);
-
   if (!author) return res.status(404).json({ message: 'Não encontrado' });
-
   res.status(200).json(author);
+})
+
+app.get('/books', async (req, res) => {
+  const books = await Book.getAll();
+  res.status(200).json(books);
+});
+
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.findById(id);
+  if (!book) return res.status(404).json({ message: 'Não encontrado' });
+  res.status(200).json(book);
 })
 
 const PORT = process.env.PORT || 3000;
--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Book.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Book.js
@@ -1,15 +1,26 @@
 const connection = require('./connection');
 
+const serialize = ({ id, title, author_id }) => ({ id, title, authorId: author_id });
+
 const getAll = async () => {
   const [books] = await connection.execute('SELECT * FROM model_example.books;');
 
-  return books.map(({ id, title, author_id }) => ({
-    id,
-    title,
-    authorId: author_id,
-  }));
+  return books.map(serialize);
 };
+
+const findById = async (id) => {
+  const query = 'SELECT id, title, author_id FROM model_example.books WHERE id = ?';
+  
+  const [bookData] = await connection.execute(query, [id]);
+
+  if (bookData.length === 0) return null;
+
+  const book = bookData.map(serialize);
+
+  return book;
+}
 
 module.exports = {
   getAll,
+  findById,
 };
