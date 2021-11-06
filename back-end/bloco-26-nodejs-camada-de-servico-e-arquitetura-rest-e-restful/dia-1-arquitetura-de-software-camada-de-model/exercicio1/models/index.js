76a2ec56bdd177670221dc5d107252518ed79f91
cristianocsl <cslcristiano@gmail.com>
2021-11-05 22:23:20 -0300

    exercicio 1 -  26.1 - cria /models/User.js

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicio1/models/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicio1/models/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
+
+app.post('/user', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+})
