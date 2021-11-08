04b162227eb1e2c0e6d2a9b86b8f75672a61bf2f
cristianocsl <cslcristiano@gmail.com>
2021-11-08 16:22:50 -0300

    exercicio 1 -  26.1

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicio1/models/User.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicio1/models/User.js
@@ -1,3 +1,5 @@
+const connection = require('./connection');
+
 const msgPassw = {
   error: true,
   message: `O campo 'password' deve ter pelo menos 6 caracteres`
@@ -18,27 +20,32 @@ const msgEmail = {
   message: `O campo 'firstName' deve ser uma string`
 }
 
+const isValid = ({ firstName, lastName, email, password }) => {
+  const PASSWORD_REGEX = /[a-z0-0]{6,}/ig;
+  const fields = [firstName, lastName, email, password];
 
-const isValidFirstName = (firstName) => {
-  if (!firstName || typeof firstName !== 'string') return msgFirstN;
+  if (fields.includes(undefined) || fields.includes(null) || fields.includes('')) return false;
+
+  return PASSWORD_REGEX.test(password);
 }
 
-const isValidLastName = (lastName) => {
-  if (!lastName || typeof lastName !== 'string')return msgLastN;
+const formatUser = (document) => {
+  const { _id, password, ...user } = document;
+  const formattedResult = { id: _id, ...user };
+  return formattedResult;
+};
 
-}
-
-const isValidEmail = (email) => {
-  if (!email || typeof email !== 'string' || email.length < 6) return msgEmail;
-}
-
-const isValidPassword = (password) => {
-  if (!password || typeof password !== 'string') return msgPassw;
+const create = ({ firstName, lastName, email, password }) => {
+  return connection()
+    .then(
+      (db) => db
+        .collection('users')
+        .insertOne({ firstName, lastName, email, password })
+    )
+    .then((result) => ({ id: result.insertedId, firstName, lastName, email }));
 }
 
 module.exports = {
-  isValidFirstName,
-  isValidEmail,
-  isValidLastName,
-  isValidPassword,
+  isValid,
+  create,
 }
