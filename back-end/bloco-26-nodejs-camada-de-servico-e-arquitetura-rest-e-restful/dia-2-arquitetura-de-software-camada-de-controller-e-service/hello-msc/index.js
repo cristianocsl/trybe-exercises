adaca558ae041a9da5e5c69facccb2379762b586
cristianocsl <cslcristiano@gmail.com>
2021-11-08 22:18:48 -0300

    refatoraçao segundo exercicio de fixaçao

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/index.js
@@ -10,4 +10,30 @@ app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
 
   res.status(200).json(authors);
-})
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { firstName, middleName, lastName } = req.body;
+
+  const author = await Author.isValid(firstName, middleName, lastName);
+
+  if (!author) return res.status(400).json({ message : 'Dados inválidos' });
+
+  await Author.create(firstName, middleName, lastName);
+
+  res.status(201).json({ message: 'Autor criado com sucesso' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
