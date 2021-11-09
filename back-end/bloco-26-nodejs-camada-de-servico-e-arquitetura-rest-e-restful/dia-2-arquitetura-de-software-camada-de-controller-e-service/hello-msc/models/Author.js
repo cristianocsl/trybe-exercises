43d1c6f043a027894ac6c85eeca372482620a5c5
cristianocsl <cslcristiano@gmail.com>
2021-11-09 00:20:54 -0300

    refatoraçao segundo exercicio de fixaçao

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/models/Author.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/models/Author.js
@@ -33,4 +33,76 @@ const getAll = async () => {
     );
 };
 
+const findById = async (id) => {
+  if (!ObjectId.isValid(id)) return null;
 
+  const authorData = await connection()
+    .then(
+      (db) => db.collection('authors')
+      .findOne(new ObjectId(id))
+    );
+  
+  if (!authorData) return null;
+  
+  const { firstName, middleName, lastName } = authorData;
+
+  return getNewAuthor({
+    id, firstName, middleName, lastName,
+  })
+}
+
+const isNonEmptyString = (value) => {
+  if (!value) return false;
+
+  return typeof value === 'string';
+};
+
+const isValid = (firstName, middleName, lastName) => {
+  if (middleName && typeof middleName !== 'string') return false;
+
+  return isNonEmptyString(firstName) && isNonEmptyString(lastName);
+};
+
+const create = async (firstName, middleName, lastName) =>
+  await connection()
+    .then(
+      (db) => db
+      .collection('authors')
+      .insertOne({ firstName, middleName, lastName })
+    .then(
+      (result) => getNewAuthor({
+        id: result.insertedId,
+        firstName,
+        middleName,
+        lastName,
+      })
+      )
+    );
+
+const findByName = async (firstName, middleName, lastName) => {
+  // Determinamos se devemos buscar com ou sem o nome do meio
+  const query = middleName
+    ? { firstName, middleName, lastName }
+    : { firstName, lastName };
+  
+  // Executamos a consulta e retornamos o resultado
+  const author = await connection()
+    .then(
+      (db) => db
+        .collection('authors')
+        .findOne(query)
+    );
+  // Caso nenhum author seja encontrado, devolvemos null
+ if (!author) return null;
+
+ // Caso contrário, retornamos o author encontrado
+ return getNewAuthor(author);
+}
+
+module.exports = {
+  getAll,
+  findById,
+  isValid,
+  create,
+  findByName,
+};
---
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/services/Author.js
@@ -0,0 +1,28 @@
+const Author = require('../models/Author');
+
+const getAll = async () => Author.getAll();
+
+const findById = async (id) => Author.findById(id);
+
+const create = async (firstName, middleName, lastName) => {
+  // Buscamos um autor com o mesmo nome completo que desejamos criar
+  const existingAuthor = await Author.findByName(firstName, middleName, lastName);
+// Caso esse autor já exista, retornamos um objeto de erro informando que não é possível criar o autor pois ele já existe
+  if (existingAuthor) {
+    return {
+      error: {
+        code: 'alreadyExists',
+        message: 'Um autor já existe com esse nome completo'
+      }
+    };
+  }
+
+
+  Author.create(firstName, middleName, lastName);
+}
+
+module.exports = {
+  getAll,
+  findById,
+  create,
+};
