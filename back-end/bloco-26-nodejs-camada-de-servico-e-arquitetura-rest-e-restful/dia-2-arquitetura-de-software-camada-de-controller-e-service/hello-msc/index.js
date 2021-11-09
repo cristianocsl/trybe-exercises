ff31c8d62a4912ffc3acad165e244372d9f9540f
cristianocsl <cslcristiano@gmail.com>
2021-11-09 16:20:06 -0300

    ajustes em ./services/Author.js

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/hello-msc/index.js
@@ -2,37 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const Author = require('./models/Author');
+const Author = require('./controllers/Author');
 
 app.use(bodyParser.json());
 
-app.get('/authors', async (_req, res) => {
-  const authors = await Author.getAll();
-
-  res.status(200).json(authors);
-});
-
-app.get('/authors/:id', async (req, res) => {
-  const { id } = req.params;
-
-  const author = await Author.findById(id);
-
-  if (!author) return res.status(404).json({ message: 'Not found' });
-
-  res.status(200).json(author);
-});
-
-app.post('/authors', async (req, res) => {
-  const { firstName, middleName, lastName } = req.body;
-
-  const author = await Author.isValid(firstName, middleName, lastName);
-
-  if (!author) return res.status(400).json({ message : 'Dados inválidos' });
-
-  await Author.create(firstName, middleName, lastName);
-
-  res.status(201).json({ message: 'Autor criado com sucesso' });
-});
+app.get('/authors', Author.getAll);
+app.get('/authors/:id', Author.findById);
+app.post('/authors', Author.create);
 
 const PORT = process.env.PORT || 3000;
 
