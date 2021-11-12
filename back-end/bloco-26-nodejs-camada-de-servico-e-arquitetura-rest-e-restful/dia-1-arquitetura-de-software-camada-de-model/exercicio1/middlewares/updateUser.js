35c46f936c9925333e834a8a24725af400e6c3c9
cristianocsl <cslcristiano@gmail.com>
2021-11-12 15:14:32 -0300

    exercicio 2 - middlewares/updateUser.js

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicio1/middlewares/updateUser.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicio1/middlewares/updateUser.js
@@ -3,6 +3,7 @@ const UserModel = require('../models/User');
 
 module.exports = rescue(async (req, res) => {
   const { id } = req.params;
+  const { firstName, lastName, email, password } = req.body;
   const user = await UserModel.findUserById(id);
 
   if (!user) return res.status(404).json({
