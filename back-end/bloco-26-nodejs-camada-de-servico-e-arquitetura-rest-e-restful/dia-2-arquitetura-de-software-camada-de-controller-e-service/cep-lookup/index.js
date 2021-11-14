57a275abbf445d99f2b3e210bd52ed869dbebd18
cristianocsl <cslcristiano@gmail.com>
2021-11-14 14:55:29 -0300

    index.js

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
@@ -1,6 +1,11 @@
+require('dotenv').config(); // as variávis de ambiente são carregadas
 const express = require('express');
-const app = express();
+const app = express(); // cria a aplicação do express
 const PORT = process.env.PORT || 3000;
+
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pong!' });
+});
 
 
 
