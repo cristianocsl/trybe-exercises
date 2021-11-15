3353e1e96f235ba35bbe61ed85ca95c37a20f90c
cristianocsl <cslcristiano@gmail.com>
2021-11-15 18:01:37 -0300

    validaçao com joi e fluxo de erro com next()

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/controllers/Cep.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/controllers/Cep.js
@@ -1,5 +1,6 @@
 const rescue = require('express-rescue');
 const CepService = require('../services/Cep');
+const Joi = require('joi');
 
 const findAddressByCep = rescue(
   async (req, res, next) => {
@@ -14,6 +15,33 @@ const findAddressByCep = rescue(
   }
 );
 
+const create = rescue(
+  async (req, res, next) => {
+    // Armazenamos essa parte do schema do Joi para reutilizá-la
+    const requireNonEmptyString = Joi.string().not.empty().required();
+    // Validação do corpo da request
+    const { error } = Joi.object({
+      cep: Joi.string().regex(/\d{5}-\d{3}/).required(),
+      logradouro: requireNonEmptyString,
+      bairro: requireNonEmptyString,
+      localidade: requireNonEmptyString,
+      uf: requireNonEmptyString.length(2),
+    }).validate(req.body);
+
+    // Caso haja erro de validação, iniciamos o fluxo de erro.
+    if (error) return next(error);
+
+    // Caso não haja erro de validação, chama a função que cria o cep.
+    const newCep = await CepService.create(req.body);
+
+    // Se o cep retornar um erro...
+    if (newCep.error) return next(newCep.error);
+
+    res.status(201).json(newCep);
+  },
+);
+
 module.exports = {
   findAddressByCep,
+  create,
 }
