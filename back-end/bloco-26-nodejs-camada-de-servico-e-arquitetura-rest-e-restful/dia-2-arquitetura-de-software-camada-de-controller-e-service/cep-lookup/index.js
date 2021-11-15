84aca7efafb64715c55cc9fbc1a2274638933d49
cristianocsl <cslcristiano@gmail.com>
2021-11-15 01:43:44 -0300

    index.js

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
@@ -1,16 +1,20 @@
 require('dotenv').config(); // as variávis de ambiente são carregadas
 const express = require('express');
 const app = express(); // cria a aplicação do express
+const bodyParser = ('body-parser');
+const CepController = require('./controllers');
+const errorMiddleware = require('./middlewares/error.js')
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong!' });
 });
 
-app.get('/cep/:cep', (req, res) => {
-  const { cep } = req.params;
+app.get('/cep/:cep', CepController.findAddressByCep);
 
-})
+app.use (errorMiddleware);
 
 
 
--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/middlewares/error.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/middlewares/error.js
@@ -5,5 +5,18 @@ module.exports = (err, req, res, _netx) => {
     }
   });
   // Verificamos se esse é um erro de domínio
-  
+  if (err.code) {
+    const statusByErrorCode = { notFound: 404 }
+    
+      const status = statusByErrorCode[err.code] || 500;
+    
+      res.status(status).json(err);
+  };
+  console.error(err);
+  res.status(500).json({
+    error: {
+      code: 'internal',
+      message: 'Internal server error',
+    }
+  })
 }
