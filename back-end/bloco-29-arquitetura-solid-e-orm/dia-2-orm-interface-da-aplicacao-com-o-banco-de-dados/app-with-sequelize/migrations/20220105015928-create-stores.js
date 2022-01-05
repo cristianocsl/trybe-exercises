81f99945b6f2107e377ed965d3c36f880f98b0bb
cristianocsl <cslcristiano@gmail.com>
2022-01-05 00:06:32 -0300

    db create

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/migrations/20220105015928-create-stores.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/migrations/20220105015928-create-stores.js
@@ -1,21 +1,30 @@
 'use strict';
 
+const sequelize = require("sequelize");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    const StoresTable = await queryInterface.createTable('Stores', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+    });
+
+    return StoresTable;
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Stores');
   }
 };
