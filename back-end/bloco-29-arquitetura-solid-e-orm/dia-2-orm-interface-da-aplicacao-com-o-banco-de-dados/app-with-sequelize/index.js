322c8b2c5c99f85f26cf5c0ed3517fe9220a3497
cristianocsl <cslcristiano@gmail.com>
2022-01-05 00:44:28 -0300

    criando model Store.js e utilizando findBYpK()

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/index.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/index.js
@@ -13,6 +13,20 @@ app.get('/', (_req, res) => {
     });
 });
 
+app.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const store = await Store.findByPk(id);
+
+    if(!store) return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(store);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Erro interno!' });
+  }
+})
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
