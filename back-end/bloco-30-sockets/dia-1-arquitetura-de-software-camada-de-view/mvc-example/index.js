a82107dc286a683946f3d001b47bd61d7d4c70b3
cristianocsl <cslcristiano@gmail.com>
2022-01-12 11:09:39 -0300

    exercicio de fixaçao]

---
+++ back-end/bloco-30-sockets/dia-1-arquitetura-de-software-camada-de-view/mvc-example/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const authorController = require('./controllers/authorController');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
+app.get('/authors', authorController.listAuthors);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
