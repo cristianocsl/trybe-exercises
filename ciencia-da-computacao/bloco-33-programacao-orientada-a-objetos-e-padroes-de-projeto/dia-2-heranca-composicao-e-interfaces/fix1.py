4df0d3f63a474f2e6513a95dd125cb07bbe2d15f
cristianocsl <cslcristiano@gmail.com>
2022-02-23 12:56:48 -0300

    fixaçao 01

--- ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-2-heranca-composicao-e-interfaces/fix1.py
+++ ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-2-heranca-composicao-e-interfaces/fix1.py
@@ -1,7 +1,8 @@
 import json
+from csv import DictWriter
 
 
-class SalesReport():  # entidade SalesReporter
+class SalesReport:  # entidade SalesReporter
     def __init__(self, export_file):  # o método construtor __init__ cria e ...
         # ...retorna um objeto.
         self.export_file = export_file + ".json"
@@ -11,7 +12,7 @@ class SalesReport():  # entidade SalesReporter
         ou seja, criar um relatório imprimível. Por simplicidade,
         vamos omitir essa lógica nos exemplos!"""
         return [
-            {
+            {  # este dicionário compõem uma linha
                 "Coluna 1": "Dado 1",
                 "Coluna 2": "Dado 2",
                 "Coluna 3": "Dado 3",
@@ -28,7 +29,19 @@ class SalesReport():  # entidade SalesReporter
         with open(self.export_file, mode="w") as file:
             json.dump(self.build(), file)
 
+    def serialize_csv(self):
+        with open("meu_relatorio.csv", mode="w") as file:
+            headers = ["Coluna 1", "Coluna 2", "Coluna 3"]
+            """O nome das colunas deve ser igual ao do return de build()"""
+            csv_writer = DictWriter(file, headers)  # objeto csv_writer
+            csv_writer.writeheader()
+            """writeheader() escreve (grava) uma linha com os nomes dos campos
+            especificados no construtor"""
+            for item in self.build():
+                # print(item)
+                csv_writer.writerow(item)
 
-meu_relatorio_de_vendas = SalesReport('meu_relatorio')
 
+meu_relatorio_de_vendas = SalesReport("meu_relatorio")
 meu_relatorio_de_vendas.serialize()
+meu_relatorio_de_vendas.serialize_csv()
