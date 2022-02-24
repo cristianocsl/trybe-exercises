d577fd9ff5e3e01e716498d58072f06443cb0694
cristianocsl <cslcristiano@gmail.com>
2022-02-24 13:38:28 -0300

    exercicios de fixaçao

--- ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-2-heranca-composicao-e-interfaces/fix4.py
+++ ciencia-da-computacao/bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-2-heranca-composicao-e-interfaces/fix4.py
@@ -1,47 +1,56 @@
 from abc import ABC, abstractmethod
-from 
 import gzip
 import json
+from zipfile import ZipFile
+
+
+class ZipCompressor:
+    """Nossos compressores terão fixado o local de salvamento
+    do arquivo, então vamos defini-lo nos construtores"""
+
+    def __init__(self, filepath="./"):
+        self.filepath = filepath
+
+    def compress(self, file_name):
+        with ZipFile(file_name + ".zip", "w") as zip_file:
+            zip_file.write(file_name)
+
+
+class GzCompressor:
+    def __init__(self, filepath="./"):
+        self.filepath = filepath
+
+    def compress(self, file_name):
+        with open(file_name, "rb") as content:
+            with gzip.open(file_name + ".gz", "wb") as gzip_file:
+                gzip_file.writelines(content)
 
 
 class SalesReport(ABC):
-    def __init__(self, export_file):
+    # Nossa classe agora espera *instâncias* de compressor como um parâmetro.
+    # Temos um valor padrão para suportar o código que usava as SalesReport
+    # sem parâmetros -- não precisa correr pra re-escrever nada ;)
+    def __init__(self, export_file, compressor=GzCompressor()):
         self.export_file = export_file
+        self.compressor = compressor
 
     def build(self):
-        """Aqui colocamos a lógica para a entidade 'se criar',
-        ou seja, criar um relatório imprimível. Por simplicidade,
-        vamos omitir essa lógica nos exemplos!
-        No contexto de Programação Orientada a Objetos,
-        pense que coisas abstratas são coisas criadas
-        para serem especializadas por classes herdeiras!"""
         return [
-            {  # este dicionário compõem uma linha
-                "Coluna 1": "Dado 1",
-                "Coluna 2": "Dado 2",
-                "Coluna 3": "Dado 3",
-            },
-            {
-                "Coluna 1": "Dado A",
-                "Coluna 2": "Dado B",
-                "Coluna 3": "Dado C",
-            },
+            {"Coluna 1": "Dado 1", "Coluna 2": "Dado 2", "Coluna 3": "Dado 3"},
+            {"Coluna 1": "Dado A", "Coluna 2": "Dado B", "Coluna 3": "Dado C"},
         ]
 
-    """O comportamento de compressão foi adicionado à classe
-    ascendente, e não à classe herdeira, pois todos os relatório
-    terão que ser comprimidos. Como isso não é um comportamento
-    especializado, foi colocado na classe ascendente."""
+    # Aqui temos um atributo de classe!
+    FILE_EXTENSION = ""
+
+    def get_export_file_name(self):
+        # Aqui usamos o atributo de classe
+        # Vai fazer mais sentido nas classes herdeiras!
+        return self.export_file + self.FILE_EXTENSION
+
     def compress(self):
-        binary_content = json.dumps(self.build()).encode("utf-8")
-
-        with gzip.open(self.export_file + ".gz", "wb") as compressed_file:
-            compressed_file.write(binary_content)
-
-    """A classe SalesReport define o método serialize para
-    deixar nítido que todo relatório de vendas deve ter
-    uma forma de se serializar, mas ela mesma, por ser
-    geral, não é serializável."""
+        self.serialize()
+        self.compressor.compress(self.get_export_file_name())
 
     @abstractmethod
     def serialize(self):
@@ -49,24 +58,41 @@ class SalesReport(ABC):
 
 
 class SalesReportJSON(SalesReport):
+    # Nós não reimplementamos o get_export_file_name
+    # mas como ele usa um atributo de classe pra pegar a extensão
+    # só de redefinir o atributo já vamos conseguir mudar o resultado!
+    FILE_EXTENSION = ".json"
+
     def serialize(self):
-        with open(self.export_file + ".json", "w") as file:
+        with open(self.get_export_file_name(), "w") as file:
             json.dump(self.build(), file)
 
 
 class SalesReportCSV(SalesReport):
-    def serialize(self):
-        with open(self.export_file + ".csv", "w") as file:
-            headers = ["Coluna 1", "Coluna 2", "Coluna 3"]
-            csv_writer = DictWriter(file, headers)
-            csv_writer.writeheader()
-            for item in self.build():
-                csv_writer.writerow(item)
+    # Sua implementação vai aqui
+    pass
 
 
-# para testar:
-relatorio_de_vendas = SalesReportJSON("novo_relatorio")
-print(relatorio_de_vendas.get_length())
+# Para testar
+relatorio_de_compras = SalesReportJSON("meu_relatorio_1")  # este relatório
+# será gerado com GzCompressor.
+# Como a classe ascendente já recebe, por padrão
+# o parâmetro compressor recebendo GzCompressor (compressor = GzCompressor),
+# não é necessário especificar.
+relatorio_de_vendas = SalesReportJSON("meu_relatorio_2", ZipCompressor())
+# neste segundo caso, optou-se por escolher outro
+# métod: ZipCompressor()
 
-relatorio_de_compras = SalesReportCSV("novo_relatorio")
-print(relatorio_de_compras.get_length())
+"""
+Cada variável (relatorio_de_compras/vendas), possui o método compress
+tanto para a classe GzCompressor quanto para a classe ZipCompressor.
+
+Nesse exemplo, o método compress deixa de ser um
+@abstractmethod.
+
+A classe ascendente cria um atributo 'compressor' que irá receber uma das
+classes GzCompressor/ZipCompressor
+
+"""
+relatorio_de_compras.compress()
+relatorio_de_vendas.compress()
