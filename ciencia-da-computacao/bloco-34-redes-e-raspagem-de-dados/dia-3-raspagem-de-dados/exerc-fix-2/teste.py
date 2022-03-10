d91c4a303664d9dc372f4b287fe6b63bda0bf15a
cristianocsl <cslcristiano@gmail.com>
2022-03-09 23:15:55 -0300

    exemplo course

---
+++ ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/exerc-fix-2/teste.py
@@ -0,0 +1,14 @@
+from parsel import Selector
+import requests
+
+URL_BASE = "http://books.toscrape.com/catalogue/"
+
+response = requests.get(URL_BASE + "page-1.html")
+
+selector = Selector(text=response.text)
+
+href = selector.css(".product_pod h3 a::attr(href)").get()
+
+print(href)
+
+print(URL_BASE + href)
