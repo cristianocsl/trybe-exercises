d6c3ce5527f00a569a191ba14a45914d85e02dcd
cristianocsl <cslcristiano@gmail.com>
2022-03-10 13:16:29 -0300

    exemplo course

--- ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/exerc-fix-2/teste.py
+++ ciencia-da-computacao/bloco-34-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/exerc-fix-2/teste.py
@@ -1,14 +1,58 @@
 from parsel import Selector
 import requests
 
-URL_BASE = "http://books.toscrape.com/catalogue/"
+# URL_BASE = "http://books.toscrape.com/catalogue/"
 
-response = requests.get(URL_BASE + "page-1.html")
+# response = requests.get(URL_BASE + "page-1.html")
+
+# selector = Selector(text=response.text)
+
+# href = selector.css(".product_pod h3 a::attr(href)").get()
+
+# print(href)
+
+# print(URL_BASE + href)
+
+"""
+Agora, para utilizar a expressão regular que fizemos, podemos substituir o
+método getall pelo método re , ou o método get por re_first . Ambos, além de
+ recuperar valores, aplicarão a expressão regular sobre aquele valor.
+
+ Essa expressão regular pode ser feita da seguinte forma: £\d+\.\d{2} .
+
+ podemos substituir o método getall pelo método re , ou o método get
+ por re_first.Ambos, além de recuperar valores, aplicarão a expressão regular
+ sobre aquele valor.
+ """
+
+# URL_BASE2 = "http://books.toscrape.com/"
+
+# response = requests.get(URL_BASE2)
+
+# selector = Selector(text=response.text)
+
+# prices = selector.css(".product_pod .price_color::text").re(r"£\d+\.\d{2}")
+
+# print(prices)
+
+URL_BASE3 = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+
+response = requests.get(URL_BASE3)
 
 selector = Selector(text=response.text)
 
-href = selector.css(".product_pod h3 a::attr(href)").get()
+description = selector.css("#product_description ~ p::text").get()
 
-print(href)
+# print(description)
 
-print(URL_BASE + href)
+# "Fatiamos" a descrição removendo o sufixo
+suffix = "...more"
+if description.endswith(suffix):
+    description = description[:-len(suffix)]
+
+# remove sufixo: str[:-len('sufixo')]
+# é o mesmo que str.removesuffix('sufixo')
+# remove prefixo: str[len('sufixo'):]
+# é o mesmo que str.removeprefix('sufixo')
+
+print(description)
