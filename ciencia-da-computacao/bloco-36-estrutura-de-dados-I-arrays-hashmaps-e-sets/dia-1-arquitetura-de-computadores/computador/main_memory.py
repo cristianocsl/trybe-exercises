ae6b321d707b4e16edf17a899425222a14a7411d
cristianocsl <cslcristiano@gmail.com>
2022-03-24 12:23:15 -0300

    exercicios dia 36.1

--- ciencia-da-computacao/bloco-36-estrutura-de-dados-I-arrays-hashmaps-e-sets/dia-1-arquitetura-de-computadores/computador/main_memory.py
+++ ciencia-da-computacao/bloco-36-estrutura-de-dados-I-arrays-hashmaps-e-sets/dia-1-arquitetura-de-computadores/computador/main_memory.py
@@ -5,13 +5,11 @@ class MainMemory:
     def load(self, value):
         self.loaded_memory.append(value)
 
-
     def get(self, index):
         try:
             return self.loaded_memory[index]
         except IndexError:
             return 0
-
 
     def clean(self):
         self.loaded_memory = []
