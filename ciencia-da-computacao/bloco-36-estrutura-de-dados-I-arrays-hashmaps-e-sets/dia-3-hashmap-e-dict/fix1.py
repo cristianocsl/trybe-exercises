344a4e7ae4c4e00e2af00a36ed2a96660dd81c60
cristianocsl <cslcristiano@gmail.com>
2022-03-29 12:52:23 -0300

    exercicios fixaçao course

--- ciencia-da-computacao/bloco-36-estrutura-de-dados-I-arrays-hashmaps-e-sets/dia-3-hashmap-e-dict/fix1.py
+++ ciencia-da-computacao/bloco-36-estrutura-de-dados-I-arrays-hashmaps-e-sets/dia-3-hashmap-e-dict/fix1.py
@@ -15,19 +15,29 @@ class HashMap:
 
     def get_address(self, id_num):
         return id_num % 10
-        """
-        Para povoar nossa hash, recebemos o objeto, computamos o seu address,
-        a partir da chave numérica, e armazenamos no local adequado.
-        """
+    """
+    Para povoar nossa hash, recebemos o objeto, computamos o seu address,
+    a partir da chave numérica, e armazenamos no local adequado.
+    """
     def insert(self, employee):
         address = self.get_address(employee.id_num)
         self._buckets[address] = employee
-        """
-        Para acessar o dado de interesse, passamos a chave. A classe
-        identifica o índice, onde a referência para aquele objeto está
-        armazenada, e retorna o valor que estiver no campo name .
-        """
+    """
+    Para acessar o dado de interesse, passamos a chave. A classe
+    identifica o índice, onde a referência para aquele objeto está
+    armazenada, e retorna o valor que estiver no campo name .
+    """
 
     def get_value(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address].name
+
+    """
+    Para consultar se uma determinada chave existe dentro da sua hash map,
+    basta calcular o address . Além disso, vamos certificar de que o conteúdo
+    da lista buckets não é None .
+    """
+
+    def has(self, id_num):
+        address = self.get_address(id_num)
+        return self._buckets[address] is not None
