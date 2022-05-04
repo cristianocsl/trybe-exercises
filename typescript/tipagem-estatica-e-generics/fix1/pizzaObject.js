d8da36257f9cad5487be42b4362aad53e7e3fe78
cristianocsl <cslcristiano@gmail.com>
2022-05-04 17:08:34 -0300

    exercícios de fixação

---
+++ typescript/tipagem-estatica-e-generics/fix1/pizzaObject.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const calabreza = {
+    flavor: 'Calabreza',
+    slices: 8,
+};
+console.log(calabreza);
+const marguerita = {
+    flavor: 'Marguerita',
+    slices: 6,
+};
+console.log(marguerita);
+const nutela = {
+    flavor: 'Nutela',
+    slices: 4,
+};
+console.log(nutela);
